e6aab7449f3a756070ce182357b131f37a12a0ae
Ler0v <robledoagustin7@gmail.com>
2022-01-25 20:38:21 -0300

    footer a jquery

---
+++ java/index/footer.js
@@ -0,0 +1,12 @@
+$("#footer").append(`<div class="card text-center letraDescripcion">
+<div class="card-header">
+  The Future is here
+</div>
+<div class="card-body">
+  <h5 class="card-title">Culture</h5>
+  <p class="card-text"></p>
+</div>
+<div class="card-footer text-muted">
+  Culture© 2021
+</div>
+</div>`)
