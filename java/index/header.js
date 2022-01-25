ecdcffdecdf0ca29ce7a57e065d87800b23ec667
Ler0v <robledoagustin7@gmail.com>
2022-01-25 20:26:09 -0300

    index jquery

---
+++ java/index/header.js
@@ -0,0 +1,31 @@
+$("#inicio").append(`<div class="barraNavegacion">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="index.html">CULTURE</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item ">
+        <a class="nav-link" href="index.html">Inicio <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="productos.html">Productos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sobrenosotros.html" tabindex="-1" aria-disabled="true">Sobre Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="preguntas.html" tabindex="-1" aria-disabled="true">Preguntas Frecuentes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contacto.html" tabindex="-1" aria-disabled="true">Contacto</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<h1 class="letraEspecial">CULTURE</h1>
+</div>`)
+
+
