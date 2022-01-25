555614bcf16e1d63aead9cf03c558970ccced7fd
Ler0v <robledoagustin7@gmail.com>
2022-01-25 20:35:45 -0300

    cards index a jquery

---
+++ java/index/imgcards.js
@@ -0,0 +1,14 @@
+$("#imgcards").append(`<div id="novedades" class="wow animate__animated animate__fadeInDown">
+<div class="content bgColorBlue fontColorMain">
+    <p class="titulo typeBold">Culture</p>
+    <p>El lugar donde el streetwear, la cultura hype, el rap, el trap, y el mundo digital se fusionan para crear algo unico</p>
+</div>
+<div class="deco"></div>
+</div>
+<div id="novedades" class="wow animate__animated animate__fadeInUp">
+<div class="deco"></div>
+<div class="content bgColorBlue fontColorMain">
+    <p class="titulo typeBold">Culture</p>
+    <p>Creativo/Moderno/Exclusivo/Productos de calidad/Desafiante/Confiabilidad/Marca amiga/Canchero</p>
+</div>
+</div>`)
