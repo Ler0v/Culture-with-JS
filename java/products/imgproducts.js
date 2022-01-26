f920c12050468ee5f6b6a29ce4012772a504c83a
Ler0v <robledoagustin7@gmail.com>
2022-01-26 00:13:17 -0300

    img products jquery

---
+++ java/products/imgproducts.js
@@ -0,0 +1,9 @@
+$("#imgproducts").append(`<h1 class="letraEspecial">CULTURE</h1>
+<div class="container-fluid">
+    <div class="row align-items-center about-us__container2">
+        <div class="col-md-7 about-us__titulo">
+            <h2 id="h2-aboutus" class='about-us__titulo-h1'>Nuestros Productos</h2>
+            <p class='about-us__titulo-p'>Hoddies</p>
+        </div>
+    </div>
+</div>`)
