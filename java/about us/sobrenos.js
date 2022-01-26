09d96dbdfa3cb33b7f7382c34521aa1d17a2dab9
Ler0v <robledoagustin7@gmail.com>
2022-01-26 00:29:15 -0300

    img aboutus jquery

---
+++ java/about us/sobrenos.js
@@ -0,0 +1,9 @@
+$("#sobrenos").append(`
+<div class="container-fluid">
+    <div class="row align-items-center about-us__container3">
+        <div class="col-md-7 about-us__titulo">
+            <h2 id="h2-aboutus" class='about-us__titulo-h1'>Preguntas Frecuentes</h2>
+            <p class='about-us__titulo-p'>Conocenos.</p>
+        </div>
+    </div>
+</div>`)
