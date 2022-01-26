ff879bb9f3b5170acbf2c8153d1840d89d4057eb
Ler0v <robledoagustin7@gmail.com>
2022-01-25 23:39:02 -0300

    barra progreso query

---
+++ java/about us/barraprogreso.js
@@ -0,0 +1,3 @@
+$("#barraprogreso").append(`<div class="progress">
+<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="200" aria-valuemin="0" aria-valuemax="100" style="width: 78%"> Creating the best to wear</div>
+</div>`)
