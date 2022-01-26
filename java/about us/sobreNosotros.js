9d800f5064e3a5eb6818d943eac1f551a57ae59f
Ler0v <robledoagustin7@gmail.com>
2022-01-25 23:38:45 -0300

    cards about us jquery

---
+++ java/about us/sobreNosotros.js
@@ -0,0 +1,208 @@
+$("#sobreNosotros").append(`		<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo1.jpg" loading="lazy" srcset="media/blo1.jpg 576w, media/blo1.jpg" alt="bloque1">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Nuestros inicios
+        </h3>
+        <p class="tarjeta__parrafo">
+            Conoce mas acerca de como comenzamos
+            <ul>
+                <li>
+                    Culture nacio simplemente como una idea.
+                </li>
+                <li>
+                    Una idea abstracta, pero llena de inspiracion.
+                </li>
+                <li>
+                    Y sobre todo, muchas ganas de crecer.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo2.jpg" loading="lazy" srcset="media/blo2.jpg 576w, media/blo2.jpg" alt="bloque2">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Colaboraciones
+        </h3>
+        <p class="tarjeta__parrafo">
+            Conoce nuestras colaboraciones mas iconicas 
+            <ul>
+                <li>
+                    Las figuras mas importantes del género urbano, cantantes de rap, trap y tambien con referentes de la comunidad digital, tales como streamers, youtubers, etc.
+                </li>
+                <li>
+                    Las marcas referentes del mundo de la moda, de la cultura hype y el streetwear.
+                </li>
+                <li>
+                    Junto a Culture, en algo unico. 
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo3.jpg" loading="lazy" srcset="media/blo3.jpg 576w, media/blo3.jpg" alt="bloque3">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Futuras colaboraciones
+        </h3>
+        <p class="tarjeta__parrafo">
+             ¿Estas pensando en una collab que la puede llegar a romper?
+            <ul>
+                <li>
+                    Si pensas en alguien con quien podamos colaborar que creas que puede ser LA colaboracion, dejanos tu propuesta aca
+                </li>
+                <li>
+                    
+                </li>
+                <li>
+                    Advertencia: nada de tibios.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo4.jpg" loading="lazy" srcset="media/blo4.jpg 576w, media/blo4.jpg" alt="bloque4">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Trabaja con nosotros
+        </h3>
+        <p class="tarjeta__parrafo">
+            ¿Queres ser parte de Culture? 
+            <ul>
+                <li>
+                    Buscamos personas compropemtidas, con ganas de crecer y apostarlo todo.
+                </li>
+                <li>
+                    Buscamos ser los mejores de los mejores, asique queremos solo a los mejores de los mejores.
+                </li>
+                <li>
+                    Si crees que sos uno en un millon, dejanos tu CV aca.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo5.jpg" loading="lazy" srcset="media/blo5.jpg 576w, media/blo5.jpg" alt="bloque5">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Club Culture
+        </h3>
+        <p class="tarjeta__parrafo">
+            Join us.
+            <ul>
+                <li>
+                    Gana puntos y accede a beneficios, lanzamientos y eventos exclusivos.
+                </li>
+                <li>
+                    Enterate de todo antes que nadie.
+                </li>
+                <li>
+                    Se parte.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo6.jpg" loading="lazy" srcset="media/blo6.jpg 576w, media/blo6.jpg" alt="bloque6">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Inversores
+        </h3>
+        <p class="tarjeta__parrafo">
+            Make a profit. Help us grow.
+            <ul>
+                <li>
+                    Accede a nuestro programa exclusivo de inversores.
+                </li>
+                <li>
+                    Conoce nuestros futuros proyectos y propone los tuyos tambien.
+                </li>
+                <li>
+                    Siempre estamos abiertos a escuchar nuevas propuestas.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo7.jpg" loading="lazy" srcset="media/blo7.jpg 576w, media/blo7.jpg" alt="bloque7">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Sustentabilidad
+        </h3>
+        <p class="tarjeta__parrafo">
+            Reducir, reutilizar, reciclar. 
+            <ul>
+                <li>
+                    Transformar los residuos en materiales de origen para fabricar nuestros productos es una de las principales formas en que podemos reducir los desechos y nuestra huella de carbono mundial.  
+                </li>
+                <li>
+                    El camino de Culture hacia cero emisiones de carbono y cero desperdicio para ayudar a proteger el futuro de la moda.
+                </li>
+                <li>
+                    Unite a nuestro viaje.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>
+<section class="contenedor">
+<div class="tarjeta">
+    <img class="tarjeta__img" src="media/blo8.jpg" loading="lazy" srcset="media/blo8.jpg 576w, media/blo8.jpg" alt="bloque8">
+    <div>
+        <h3 class="tarjeta__titulo">
+            Noticias
+        </h3>
+        <p class="tarjeta__parrafo">
+            No seas el ultimo en enterarte. 
+            <ul>
+                <li>
+                    No te pierdas nada de lo que pasa en nuestro mundo.
+                </li>
+                <li>
+                    Tu fuente de las últimas historias de Culture.
+                </li>
+                <li>
+                    Las novedades del mundo de la moda, el hype, el streetwear y el mundo digital, todo aca.
+                </li>
+            </ul>
+        </p>
+        <a href="#" class="boton boton--derecha">
+            Ver más
+        </a>
+    </div>
+</section>`)
