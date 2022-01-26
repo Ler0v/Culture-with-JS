008f4d76976e44d83a1b7c474f83a9946720de60
Ler0v <robledoagustin7@gmail.com>
2022-01-25 23:47:35 -0300

    img header jquery

---
+++ java/contact/imgcontacto.js
@@ -0,0 +1,9 @@
+$("#imgcontacto").append(`<h1 class="letraEspecial">CULTURE</h1>
+<div class="container-fluid">
+    <div class="row align-items-center about-us__container4">
+        <div class="col-md-7 about-us__titulo">
+            <h2 id="h2-aboutus" class='about-us__titulo-h1'>Contacto</h2>
+            <p class='about-us__titulo-p'>Contactanos</p>
+        </div>
+    </div>
+</div>`)
