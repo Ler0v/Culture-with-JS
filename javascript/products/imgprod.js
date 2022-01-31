65e5bbcb0178925d73e9f38ee8992736fe7af32b
Ler0v <robledoagustin7@gmail.com>
2022-01-31 16:29:08 -0300

    fade animado para secciones

--- javascript/products/imgprod.js
+++ javascript/products/imgprod.js
@@ -1,10 +1,14 @@
-$("#imgprod").append(`<div class="container-fluid">
+$("#imgprod").append(`<div id= "animado" style= "display: none" class="container-fluid">
 <div class="row align-items-center about-us__container2">
     <div  class="col-md-7 about-us__titulo">
-        <h2 id="h2-aboutus" style= "display: none" class='about-us__titulo-h1'>Nuestros Productos</h2>
-        <p class='about-us__titulo-p'>Hoddies</p>
+        <h2 id="texto1" style= "display: none" class='about-us__titulo-h1'>Nuestros Productos</h2>
+        <p id="texto2" style= "display: none" class='about-us__titulo-p'>Hoddies</p>
     </div>
 </div>
 </div>`)
 
-$("#imgprod").fadeOut(3000);
+$("#animado").fadeIn(5000);
+
+$("#texto1").fadeIn(3000)
+
+$("#texto2").fadeIn(5000)
